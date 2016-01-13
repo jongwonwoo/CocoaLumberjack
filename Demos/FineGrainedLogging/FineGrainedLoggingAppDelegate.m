e62440f94eacf1eb4b83c83dd54491beb2491d02
Jongwon Woo <jongwonwoo1@gmail.com>
2016-01-13 15:50:54 +0900

    Logging to multiple files.

--- Demos/FineGrainedLogging/FineGrainedLoggingAppDelegate.m
+++ Demos/FineGrainedLogging/FineGrainedLoggingAppDelegate.m
@@ -1,6 +1,7 @@
 #import "FineGrainedLoggingAppDelegate.h"
 
 #import "MYLog.h"
+#import "DDContextFilterLogFormatter.h"
 
 #import "TimerOne.h"
 #import "TimerTwo.h"
@@ -16,6 +17,25 @@ static const DDLogLevel ddLogLevel = DDLogLevelVerbose;
 {
     [DDLog addLogger:[DDASLLogger sharedInstance] withLevel:DDLogLevelVerbose | LOG_FLAG_TIMERS];
     [DDLog addLogger:[DDTTYLogger sharedInstance] withLevel:DDLogLevelVerbose | LOG_FLAG_TIMERS];
+    
+    NSString *appName = [[NSProcessInfo processInfo] processName];
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+    NSString *basePath = ([paths count] > 0) ? paths[0] : NSTemporaryDirectory();
+    NSString *logsDirectory = [[basePath stringByAppendingPathComponent:@"Logs"] stringByAppendingPathComponent:appName];
+    
+    DDLogFileManagerDefault *fileManagerForFoodTimer = [[DDLogFileManagerDefault alloc] initWithLogsDirectory:[logsDirectory stringByAppendingPathComponent:@"FoodTimer"]];
+    DDFileLogger *loggerForFoodTimer = [[DDFileLogger alloc] initWithLogFileManager:fileManagerForFoodTimer];
+    DDContextWhitelistFilterLogFormatter *formatterForFoodTimer = [[DDContextWhitelistFilterLogFormatter alloc] init];
+    [formatterForFoodTimer addToWhitelist:LOG_CONTEXT_FOOD_TIMER];
+    [loggerForFoodTimer setLogFormatter:formatterForFoodTimer];
+    [DDLog addLogger:loggerForFoodTimer withLevel:DDLogLevelVerbose | LOG_FLAG_TIMERS];
+    
+    DDLogFileManagerDefault *fileManagerForSleepTimer = [[DDLogFileManagerDefault alloc] initWithLogsDirectory:[logsDirectory stringByAppendingPathComponent:@"SleepTimer"]];
+    DDFileLogger *loggerForSleepTimer = [[DDFileLogger alloc] initWithLogFileManager:fileManagerForSleepTimer];
+    DDContextWhitelistFilterLogFormatter *formatterForSleepTimer = [[DDContextWhitelistFilterLogFormatter alloc] init];
+    [formatterForSleepTimer addToWhitelist:LOG_CONTEXT_SLEEP_TIMER];
+    [loggerForSleepTimer setLogFormatter:formatterForSleepTimer];
+    [DDLog addLogger:loggerForSleepTimer withLevel:DDLogLevelVerbose | LOG_FLAG_TIMERS];
     
     timerOne = [[TimerOne alloc] init];
     timerTwo = [[TimerTwo alloc] init];
--- Demos/FineGrainedLogging/MYLog.h
+++ Demos/FineGrainedLogging/MYLog.h
@@ -1,13 +1,16 @@
 #import <CocoaLumberjack/CocoaLumberjack.h>
 
-// The first 4 bits are being used by the standard levels (0 - 3) 
+// The first 4 bits are being used by the standard levels (0 - 3)
 // All other bits are fair game for us to use.
 
 #define LOG_FLAG_FOOD_TIMER  (1 << 5) // 0...0100000
 #define LOG_FLAG_SLEEP_TIMER (1 << 6) // 0...1000000
 
-#define DDLogFoodTimer(frmt, ...)  LOG_MAYBE(YES, ddLogLevel, LOG_FLAG_FOOD_TIMER,  0, nil, __PRETTY_FUNCTION__, frmt, ##__VA_ARGS__)
-#define DDLogSleepTimer(frmt, ...) LOG_MAYBE(YES, ddLogLevel, LOG_FLAG_SLEEP_TIMER, 0, nil, __PRETTY_FUNCTION__, frmt, ##__VA_ARGS__)
+#define LOG_CONTEXT_FOOD_TIMER 1
+#define LOG_CONTEXT_SLEEP_TIMER 2
+
+#define DDLogFoodTimer(frmt, ...)  LOG_MAYBE(YES, ddLogLevel, LOG_FLAG_FOOD_TIMER,  LOG_CONTEXT_FOOD_TIMER, nil, __PRETTY_FUNCTION__, frmt, ##__VA_ARGS__)
+#define DDLogSleepTimer(frmt, ...) LOG_MAYBE(YES, ddLogLevel, LOG_FLAG_SLEEP_TIMER, LOG_CONTEXT_SLEEP_TIMER, nil, __PRETTY_FUNCTION__, frmt, ##__VA_ARGS__)
 
 // Now we decide which flags we want to enable in our application
 
